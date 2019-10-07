937694fa1669d63949b969c6b546566f790da76a
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-07 19:45:45 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,26 +1,45 @@
 class Dog
-  attr_accessor :name, :breed, :id
+  attr_accessor :name, :breed
+  attr_reader :id
 
-  def initialize(id: nil, name:, breed:)
+  def initialize (id: nil, name:, breed:)
     @id = id
     @name = name
     @breed = breed
   end
 
   def self.create_table
-      sql = <<-SQL
-        CREATE TABLE IF NOT EXISTS dogs (
-          id INTEGER PRIMARY KEY,
-          name TEXT,
-          breed TEXT
-          )
-        SQL
-      DB[:conn].execute(sql)
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
   end
 
   def self.drop_table
-    sql = "DROP TABLE IF EXISTS dogs"
+    sql = <<-SQL
+      DROP TABLE IF EXISTS dogs
+    SQL
     DB[:conn].execute(sql)
+  end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
+  end
+
+  def update
+    sql = <<-SQL
+      UPDATE dogs
+      SET name = ?, breed = ?
+      WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
   def save
@@ -34,29 +53,13 @@ class Dog
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
-    self
+    self # returns an instance of the Dog class
   end
 
-  def update
-    sql = <<-SQL
-      UPDATE dogs
-      SET name = ?, breed = ?
-      WHERE id = ?
-    SQL
-    DB[:conn].execute(sql, self.name, self.breed, self.id)
-  end
-
-  def self.create(name:, breed:) # gotta use those keywords!
-    dog = Dog.new(name: name,breed: breed)
+  def self.create(name:, breed:)
+    dog = Dog.new(name: name, breed: breed)
     dog.save
     dog
-  end
-
-  def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    breed = row[2]
-    self.new(id: id, name: name, breed: breed) #gotta use the keywords!
   end
 
   def self.find_by_id(id)
@@ -66,7 +69,7 @@ class Dog
       WHERE id = ?
       LIMIT 1
     SQL
-    result = DB[:conn].execute(sql, id).map do |row|
+    DB[:conn].execute(sql, id).map do |row|
       self.new_from_db(row)
     end.first
   end
@@ -82,7 +85,7 @@ class Dog
 
     if !dog.empty?
       dog_data = dog[0]
-      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2]) # keywords!
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
       dog = self.create(name: name, breed: breed)
     end
@@ -90,14 +93,18 @@ class Dog
   end
 
   def self.find_by_name(name)
-      sql = <<-SQL
-        SELECT *
-        FROM dogs
-        WHERE name = ?
-        LIMIT 1
-      SQL
-      result = DB[:conn].execute(sql, name).map do |row|
-        self.new_from_db(row)
-      end.first
-    end
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      Where name = ?
+      LIMIT 1
+    SQL
+    result = DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+
+
+
 end
